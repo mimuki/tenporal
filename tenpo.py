004697ccf046b89a4197975eb2c12baffe0be389
mimuki <60542090+mimuki@users.noreply.github.com>
2023-11-08 15:36:37 +1030

    Make it just do the current time

--- tenpo.py
+++ tenpo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Check if there's a config file
 try: 
-    from config import timezone, latitude, longitude, daysToCalculate
+    from config import timezone, latitude, longitude
 # Generate a placeholder if there isn't
 except ModuleNotFoundError:
     print("Could not find config.py.")
@@ -13,7 +13,7 @@ except ModuleNotFoundError:
 timezone  = 'Australia/Sydney' 
 latitude  = -33.86514 * N 
 longitude = 151.20990 * E
-daysToCalculate = 3""")
+""")
     config.close()
     print("You need to manually edit that file, unless you want my placeholder data.")
     exit()
@@ -27,8 +27,8 @@ from skyfield.api import wgs84, load
 # (generated when running this for the first time)
 tz = ZoneInfo(timezone)
 place = wgs84.latlon(latitude, longitude)
-days = daysToCalculate # How many days of data you want
 debug = False # Verify the calculated hours match actual sunrise/sunset
+found = False # used for "yep that's the right time, we can stop now"
 
 # Convert a datetime into seconds since midnight
 def toSeconds(list):
@@ -41,7 +41,7 @@ def toSeconds(list):
 now = dt.datetime.now().astimezone(tz)
 midnightStart = now.replace(hour=0, minute=0, second=0, microsecond=0)
 # we +1 days, otherwise we wouldn't get night hour info for the last day
-midnightEnd = midnightStart + dt.timedelta(days=(days+1))
+midnightEnd = midnightStart + dt.timedelta(days=2)
 
 # I don't totally understand this, but it works
 ts = load.timescale()
@@ -52,67 +52,81 @@ t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, place))
 # Convert to local time
 t = t.astimezone(tz)
 
-# Split sunrises and sunsets into seperate lists
-sunrises, sunsets = t[::2], t[1::2]
-dayMoments = []
-nightMoments = []
+# seconds in a day
+dayDuration = toSeconds(t[1]) - toSeconds(t[0])
+# post sunset + pre sunrise, because a night passes over midnight
+nightDuration = 86400 - toSeconds(t[1]) + toSeconds(t[3]) 
 
-# For each day requested
-for i in range (days):
-    # seconds in a day
-    dayDuration = toSeconds(sunsets[i]) - toSeconds(sunrises[i])
-    # post sunset + pre sunrise, because a night passes over midnight
-    nightDuration = 86400 - toSeconds(sunsets[i]) + toSeconds(sunrises[i+1]) 
+# Seconds per hour
+dayHourLength = dayDuration / 12
+nightHourLength = nightDuration / 12
+# Seconds per moment 
+dayMomentLength = dayHourLength / 40
+nightMomentLength = nightHourLength / 40
+# A list for each hour of the day
+moments  = [[], [], [], [], [], [], [], [], [], [], [], [],
+            [], [], [], [], [], [], [], [], [], [], [], []]
+# The start times of each hour: we know the first one is sunrise/set
+hourTime = [t[0], "", "", "", "", "", "", "", "", "", "", "",
+            t[1], "", "", "", "", "", "", "", "", "", "", ""]
 
-    # Seconds per hour
-    dayHourLength = dayDuration / 12
-    nightHourLength = nightDuration / 12
-    # Seconds per moment 
-    # TODO: implement these
-    dayMomentLength = dayHourLength / 40
-    nightMomentLength = nightHourLength / 40
-    dayMoments.append([])
-    nightMoments.append([])
-    # The start times of each hour: we know the first one is sunrise/set
-    dayHourTime = [sunrises[i]]
-    nightHourTime = [sunsets[i]]
 
-    print(sunrises[i].strftime("\n%b %d"))
-    # Make sure your math lines up with the actual sunrise and sunset
-    if debug:
-        print("    Sunrise    Sunset")
-        print(
-                sunrises[i].strftime("    %I:%M:%S %p") + "   " + 
-                sunsets[i].strftime("%I:%M:%S %p"))
+# Make sure your math lines up with the actual sunrise and sunset
+if debug:
+    print(t[0].strftime("\n%b %d"))
+    print("    Sunrise    Sunset")
+    print(t[0].strftime("    %I:%M:%S %p") + "   " + t[1].strftime("%I:%M:%S %p"))
 
     print("    Day hours     Night hours")
-    for hour in range(12):
-        if hour < 11: # make sure xHourtime[] only has 12 things in it
-            # Make the next hour equal 
-            # the current hour + the number of seconds in an hour
-            dayHourTime.append(
-                    dayHourTime[hour] + dt.timedelta(seconds = dayHourLength))
-            nightHourTime.append(
-                    nightHourTime[hour] + dt.timedelta(seconds = nightHourLength))
-
-        dayInfo = dayHourTime[hour].strftime("%I:%M:%S %p")
-        nightInfo = nightHourTime[hour].strftime("%I:%M:%S %p")
+for hour in range(12):
+    if hour < 11: # make sure xHourtime[] only has 12 things in it
+        # Make the next hour equal 
+        # the current hour + the number of seconds in an hour
+        hourTime[hour+1] = hourTime[hour] + dt.timedelta(seconds = dayHourLength)
+        hourTime[hour+13] = hourTime[hour+12] + dt.timedelta(seconds = nightHourLength)
+    if debug: 
+        dayInfo = hourTime[hour].strftime("%I:%M:%S %p")
+        nightInfo = hourTime[hour+12].strftime("%I:%M:%S %p")
         print(str(hour+1).zfill(2) + "  " + dayInfo + "   " + nightInfo)
-        # Add a list for each hour of the day/night
-        dayMoments[i].append([])
-        nightMoments[i].append([])
-
-        # Add the time of each moment to the list we just made for it's hour
-        for moment in range(40):
-            dayMoments[i][hour].append(
-                    dayHourTime[hour] + 
-                    dt.timedelta(seconds = dayMomentLength*(moment)))
-            nightMoments[i][hour].append(
-                    nightHourTime[hour] + 
-                    dt.timedelta(seconds = nightMomentLength*(moment)))
-            if debug:
-                print(str(moment+1).zfill(2) + "  "  + 
-                      dayMoments[i][hour][moment].strftime("%I:%M:%S") + "      " + 
-                      nightMoments[i][hour][moment].strftime("%I:%M:%S"))
 
 
+    # Add the time of each moment to the list we just made for it's hour
+    for moment in range(40):
+        moments[hour].append(
+                hourTime[hour] + 
+                dt.timedelta(seconds = dayMomentLength*(moment)))
+        moments[hour+12].append(
+                hourTime[hour+12] + 
+                dt.timedelta(seconds = nightMomentLength*(moment)))
+        if debug:
+            print(str(moment+1).zfill(2) + "  "  + 
+                  moments[hour][moment].strftime("%I:%M:%S") + "      " + 
+                  moments[hour][moment].strftime("%I:%M:%S"))
+hourCount = 0
+momentCount = 0
+
+for hour in hourTime:
+    if moments[hourCount][-1] <= now:
+        # Skip hour, because the last moment in it is in the past
+        hourCount = hourCount + 1
+    else:
+        # The current time is inside the current hour
+        currentHour = hour
+        lastMoment = moments[hourCount][0]
+        while not found:
+            for moment in moments:
+                if moments[hourCount][momentCount] <= now:
+                    # This hour is in the past
+                    lastMoment = moment
+                else:
+                    # This hour is in the future
+                    # which means the last one is the current one
+                    currentMoment = lastMoment
+                    momentCount = momentCount - 1
+                    found = True
+                    break
+                momentCount = momentCount + 1
+        break
+
+print(str(hourCount).zfill(2) + ":" +  str(momentCount).zfill(2))
+        
